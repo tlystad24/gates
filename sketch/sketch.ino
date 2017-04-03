71b0de02aa3358dc0416eba49353f3d8b1272dfb
Tobias Lystad <tobias24@me.com>
2017-04-03 11:54:24 +0200

    Fix syntax error

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,15 +1,15 @@
 /* Logiske kretser med arduino */
-int led1 = 11 // Første led på pin 11
-int led2 = 12 // Andre led på pin 12
+int led1 = 11; // Første led på pin 11
+int led2 = 12; // Andre led på pin 12
 
-void setup {
+void setup() {
 
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 
 }
 
-void void loop() {
+void loop() {
 
 
 
