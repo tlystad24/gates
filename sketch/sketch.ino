0aef38321cc84fd31ca28f6d6313e418cb4661af
Tobias Lystad <tobias24@me.com>
2017-04-03 13:27:02 +0200

    Change pins to 7 and 8

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,16 +1,18 @@
 /* Logiske kretser med arduino */
-int led1 = 11; // Første led på pin 11
-int led2 = 12; // Andre led på pin 12
+const int led1 = 7; // Første led på pin 7
+const int led2 = 8; // Andre led på pin 8
 
 void setup() {
-
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
+  
+  pinMode(led1, OUTPUT); // Sette led1 pin som utgang
+  pinMode(led2, OUTPUT); // Sette led2 pin som utgang
 
 }
 
 void loop() {
-
-
+  
+  // AND GATE - begge innganger er HIGH:
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
 
 }
