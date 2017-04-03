b715a004adeadf3e2d3b03551c9217ae45551de4
Tobias Lystad <tobias24@me.com>
2017-04-03 11:20:39 +0200

    Clean up

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,16 @@
 /* Logiske kretser med arduino */
-int led1 = 11
-int led2 = 12
+int led1 = 11 // Første led på pin 11
+int led2 = 12 // Andre led på pin 12
+
 void setup{
+
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
+
+}
+
+void void loop() {
+
+
+
 }
