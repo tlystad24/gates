f1aa409760430b40c3c4f7a1453458778f9a0058
Tobias Lystad <tobias24@me.com>
2017-04-03 14:12:05 +0200

    Add all four stages and upload diagram

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,18 +1,41 @@
-/* Logiske kretser med arduino */
-const int led1 = 7; // Første led på pin 7
-const int led2 = 8; // Andre led på pin 8
+/* Logiske kretser med arduino
+
+Dette programmet går igjennom alle stadiene i en AND gate:
+- 1 & 1
+- 0 & 1
+- 1 & 0
+- 0 & 0
+
+*/
+const int IC1 = 7; // Første led på pin 7
+const int IC2 = 8; // Andre led på pin 8
 
 void setup() {
   
-  pinMode(led1, OUTPUT); // Sette led1 pin som utgang
-  pinMode(led2, OUTPUT); // Sette led2 pin som utgang
+  pinMode(IC1, OUTPUT); // Sette IC1 pin som utgang
+  pinMode(IC2, OUTPUT); // Sette IC2 pin som utgang
 
 }
 
 void loop() {
   
-  // AND GATE - begge innganger er HIGH:
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
+  // Begge inngangene i IC er HIGH = led vil lyse:
+  digitalWrite(IC1, HIGH);
+  digitalWrite(IC2, HIGH);
+  delay(1000);
 
+  // Inngang 1 er LOW, og inngang 2 er HIGH = led vil ikke lyse:
+  digitalWrite(IC1, LOW);
+  digitalWrite(IC2, HIGH);
+  delay(1000);
+ 
+  // Inngang 1 er HIGH, og inngang 2 er LOW = led vil ikke lyse:
+  digitalWrite(IC1, HIGH);
+  digitalWrite(IC2, LOW);
+  delay(1000);
+
+  // Begge inngangene er LOW = led vil ikke lyse
+  digitalWrite(IC1, LOW);
+  digitalWrite(IC2, LOW);
+  delay(1000);
 }
